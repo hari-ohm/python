1675f91dc6ddf79e54bca7d21dbcc97134855280
Hariom <om.hari1215@gmail.com>
2025-07-05 02:20:24 +0530

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,12 @@
+name="hariom"
+age=26
+height=5.7
+
+#If you want to specify the data type of a variable, this can be done with casting.
+a=str(3)     #it will be "3" or '3'
+b=int(3)     #it will be 3
+c=float(3)   #it will be 3.0
+
+#to print the values
+print(name)
+print(age)
